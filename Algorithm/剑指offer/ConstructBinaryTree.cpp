cd56b0ff6eda15149e025ec67a0e5bf13284c94c
ChenwxJay <chenwx_jay@163.com>
2019-01-26 23:59:22 +0800

    增加两道题解答

--- Algorithm/剑指offer/ConstructBinaryTree.cpp
+++ Algorithm/剑指offer/ConstructBinaryTree.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class TreeNode{
+public:
+	TreeNode(int value):value_(value){
+        cout << "Construct a TreeNode!" << endl;
+	}
+	~TreeNode(){}
+private:
+	int value;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+TreeNode* ConstructBinaryTree(vector<int> pre,vector<int> middle){
+	//获取数组元素长度
+	int len = middlen.size();
+    //错误检验，判空
+    if(len <= 0){
+    	return nullptr;
+    }
+    //需要重新构建整棵树，因此需要动态分配内存
+    TreeNode* root = new TreeNode(pre[0]); //前序数组第一个节点一定是树根节点
+    //循环遍历中序遍历序列，找到根节点位置
+    int head = 0;
+    for(int i = 0;i < len;i++) //这里可以做一个改进，遍历时间复杂度为O(n)
+    {
+    	if(middle[i] == pre[0]){
+    		head = i; //需要用一个中间变量来记录根节点的下标
+    		break;
+    	}
+    	
+    }
+    //区分左右子树
+    for(int i = 0;i < index;i++){
+    	left_pre.push(pre[i+1]); //注意左子树的根节点是整棵树的根节点，因此根节点不用添加
+    	left_in.push(middle[i]);
+    }
+    //index指向根节点，可以不用添加
+    for(int j = index+1;j < len;j++){
+        right_pre.push(pre[j]) //前序遍历序列的右子树，直接添加
+        right_in.push(middle[j]);//中序遍历数组，右半部分，直接添加
+    }
+   
+   //分组之后继续递归，在第一层函数处完成设置指针
+    root->left = ConstructBinaryTree(left_pre,left_in);
+    root->right = ConstructBinaryTree(right_pre,right_in);
+
+    //最后返回当前树的根节点
+    return root;
+};
+
 int main(){
 	
 }
--- Algorithm/剑指offer/UseTwoStacksToCreateQueue.cpp
+++ Algorithm/剑指offer/UseTwoStacksToCreateQueue.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
 #include <stack>
 #include <functional>
+#include <exception>
 using namespace std;
+//两个栈实现一个队列，使用STL组件
+class StacksForQueue{
+private:
+	//底层包含了两个STL的栈对象，注意模板参数
+	stack<int> stack1;
+	stack<int> stack2;
+public:
+    //实现队列提供的方法
+    void push(int value){
+    	//直接压入栈1
+        stack1.push(value);
+    }
+    //从队头删除元素
+    int pop(){
+        //首先校验两个栈是否都为空
+        if(stack1.empty() && stack2.empty()){
+        	  //队列没有数据，直接抛出异常
+              throw new exception();
+        }
+        //如果栈2不为空，则直接pop出栈2的栈顶元素，即队头元素
+        if(!stack2.empty()){
+        	return stack2.pop();
+        }
+        //栈2为空，则需要将栈1的数据pop出来，再push到栈2，再返回
+
+        if(stack1.size() == 1) //只有一个元素，直接pop并返回即可
+        	return stack1.pop();
+        //循环处理
+        while(!stack1.empty()){
+        	//从栈1pop出数据，然后将数据push到栈2
+        	stack2.push(stack1.pop());
+        }
+        //最后需要对栈2进行pop,获取栈顶元素，相当于获取队列头元素
+        return stack2.pop();
+    }
+};
+//两个队列实现一个栈
+class QueuesForStack{
+public:
+	queue<int> queue1;
+	queue<int> queue2;
+}
+int main(){
+
+}
