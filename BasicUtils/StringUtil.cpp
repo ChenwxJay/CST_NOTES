8552146dec6ed056238252f0122dfc441720bcc4
ChenwxJay <chenwx_jay@163.com>
2019-02-03 23:59:57 +0800

    memmove函数实现

--- BasicUtils/StringUtil.cpp
+++ BasicUtils/StringUtil.cpp
@@ -29,3 +29,26 @@ int atoi(const char* str){
 	assert(str != nullptr);
     
 }
+//注意len是字节数，需要使用sizeof求解
+void* memmove(void* dst,const void* src,size_t len)
+{   
+	//保证源缓冲区和目的缓冲区不为空
+	assert(dst != NULL && src != NULL);
+	//因为需要按字节拷贝，所以定义两个临时的char*变量用于运算
+	char* temp = (char*)dst;
+	char* s_temp = (char*)src; 
+	if(dst <= src || (char*)dst - (char*)src >= len){
+          //不会发生内存重写
+          while(len--)
+          	*temp++ = *src++;
+	}
+	else{
+		//考虑内存重叠的情况，从后面开始复制
+		s_temp = (char*)src + len;
+		temp = (char*)dst + len; //偏移，移动len个字节
+		while(len--)
+			*(--temp) = *(--s_temp);//从后面开始拷贝
+	}
+	//最后需要返回目标缓冲区的首地址
+	return dst;
+}
